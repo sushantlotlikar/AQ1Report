069b10fb5737aec55c082b7e658da36bde519529
sushantlotlikar <sushpl@yahoo.com>
2015-10-11 23:14:17 +0530

    1. Removed hard-coding of input parameters.
    2. Added samples for reference.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,32 @@ namespace AQ1Report
     {
         static void Main(string[] args)
         {
-            String fileNameAQ1 = @"C:\wip\Didi\AQ 1 317 JUNE 15-ZamZar.xlsx";
-            String outputFileName = @"C:\wip\Didi\AQ1-Consolidated.xlsx";
+            String inputFileName, outputFileName;
 
-            Console.WriteLine("ProcessAQ1Excel....");
-            clsAQ1ReportBC.ProcessAQ1Excel(fileNameAQ1, outputFileName);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("PROGRAM INFORMATION:");
+            Console.ResetColor();
+            Console.WriteLine("This program takes 1 excel file (called AQ1 Report) as input, extracts information from it and generates a new excel file based on the extracted information.");
+            Console.WriteLine("INPUT: AQ1 Report (xlsx file) containing multiple worksheets.");
+            Console.WriteLine("OUTPUT: Excel (xlsx) file with a single worksheet containing all the information extracted from the input file.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("INPUT PARAMETERS:");
+            Console.ResetColor(); 
+            Console.WriteLine("Input Parameter 1: Please provide complete path to input excel file (AQ1 Report):");
+            inputFileName = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Input Parameter 2: Please provide complete path to output excel file:");
+            outputFileName = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("PROCESSING:");
+            Console.ResetColor();
+            clsAQ1ReportBC.ProcessAQ1Excel(inputFileName, outputFileName);
         }
     }
 }
